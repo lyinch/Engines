4e4f08aeaf3129c8aa1d80d240e8f2fd93812aed
lyinch <lyinch@lyinch.com>
2017-03-22 23:49:09 +0100

    added complete shader overhead

---
+++ Curiosity2D/src/shaders/WorldShader.java
@@ -0,0 +1,7 @@
+package shaders;
+
+/**
+ * Created by backes on 22/03/17.
+ */
+public class WorldShader {
+}
---
+++ Curiosity2D/src/utils/Math.java
@@ -0,0 +1,7 @@
+package utils;
+
+/**
+ * Created by backes on 22/03/17.
+ */
+public class Math {
+}
